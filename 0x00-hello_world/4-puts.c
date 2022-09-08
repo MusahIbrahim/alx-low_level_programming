b9bcce038ebf87b3d047ef0190e2bfc4376680e2
Musah Ibrahim <mark.ibm.musah@gmail.com>
2022-09-08 15:07:24 -0700

    4 puts .c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,9 @@
 #include<stdio.h>
-
 /**
+ * main - Entry point
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+ * Return: Always 0 (Succcess)
+ */
 int main(void){
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);

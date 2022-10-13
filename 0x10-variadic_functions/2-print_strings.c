14f366049d62f57ae3110ce88c5eac64b1b676f9
Musah Ibrahim <mark.ibm.musah@gmail.com>
2022-10-13 09:49:26 -0700

    2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,30 +1,3 @@
-
-Skip to content
-
-    Pricing
-
-Sign in
-Sign up
-luischaparroc /
-holbertonschool-low_level_programming
-Public
-
-Code
-Issues
-Pull requests 2
-Actions
-Projects
-Security
-
-    Insights
-
-holbertonschool-low_level_programming/0x10-variadic_functions/2-print_strings.c
-@luischaparroc
-luischaparroc Fix: it prints the new line
-Latest commit 540c1d0 on 27 Jul 2019
-History
-1 contributor
-34 lines (28 sloc) 572 Bytes
 #include "variadic_functions.h"
 
 /**
